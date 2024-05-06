eb3221cbb84f2b124a75a02d9a1388ccb98764b1
Estelle van-anh beverley EGANA <estegana@paul-f3Ar4s9.clusters.42paris.fr>
2024-05-06 19:39:00 +0200

    prompt

--- main_process.c
+++ main_process.c
@@ -6,7 +6,7 @@
 /*   By: estegana <estegana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 14:46:44 by estegana          #+#    #+#             */
-/*   Updated: 2024/05/06 15:49:27 by estegana         ###   ########.fr       */
+/*   Updated: 2024/05/06 18:55:27 by estegana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,16 @@
 
 int	main_process(void)
 {
-	printf("int ac : %d\n", s()->ac);
-	printf("char 1 av par ex : %s\n", s()->av[1]);
-	printf("char 2 env par ex: %s\n", s()->env[2]);
+	char	cmd[10];
+
+	while (1)
+	{
+		printf("minishell > ");
+		if (fgets(cmd, sizeof(cmd), stdin) == NULL)
+		{
+			printf("Erreur lors de la lecture de la commande.\n");
+			continue;
+		}
+	}
 	return (0);
 }
