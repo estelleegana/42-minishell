8b5eb3a7b91ea7e6cd25d571190ab17a1f20f655
Estelle van-anh beverley EGANA <estegana@paul-f5Ar2s3.clusters.42paris.fr>
2024-06-28 20:31:16 +0200

    cd

--- b_cd.c
+++ b_cd.c
@@ -6,7 +6,7 @@
 /*   By: estegana <estegana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 11:59:46 by estegana          #+#    #+#             */
-/*   Updated: 2024/06/12 18:24:12 by estegana         ###   ########.fr       */
+/*   Updated: 2024/06/28 20:30:26 by estegana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,19 @@
 
 int	ft_cd(void)
 {
+	//unsigned int	i;
+
+	//i = 0;
+	if (s()->p.ntokens == 1)
+		chdir("/mnt/nfs/homes/estegana");
+	else if (s()->p.ntokens == 2)
+	{
+		if (*s()->p.tokens[1] == '/')
+			chdir(s()->p.tokens[1]);
+		else
+			printf("cd 1 only a relative\n");
+	}
+	else if (s()->p.ntokens > 2)
+		printf("cd: too many args\n");
 	return (0);
 }
--- p_parsing.c
+++ p_parsing.c
@@ -6,7 +6,7 @@
 /*   By: estegana <estegana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 17:21:38 by estegana          #+#    #+#             */
-/*   Updated: 2024/06/20 17:19:12 by estegana         ###   ########.fr       */
+/*   Updated: 2024/06/28 18:48:29 by estegana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ int	ft_parsing(void)
 {
 	printf("\n------------ ENTREE DANS LE PARSING ------------\n");
 	printf("p.input = %s\n", s()->p.input);
-	ft_strtok();
-	//s()->p.tokens = ft_split(s()->p.input, ' ');//simple split par SPACE si jamais ft_strtok marche pas
+	//ft_strtok();
+	s()->p.tokens = ft_split(s()->p.input, ' ');//simple split par SPACE si jamais ft_strtok marche pas
 	ft_ntokens();
 	ft_npipes();
 	return 0;
